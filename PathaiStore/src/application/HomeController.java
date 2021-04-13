747887eb43e3a498112eb593a59640ad87a6050d
Maisha <officiallymaisha@gmail.com>
2021-04-14 05:02:00 +0600

    all scene changing methods done

--- PathaiStore/src/application/HomeController.java
+++ PathaiStore/src/application/HomeController.java
@@ -1,13 +1,136 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
-//import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+//import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class HomeController {
-	public void dum(ActionEvent ev) {
-		
+	private Stage stage;
+
+	// ============= SCENE CHANGING METHODS ===========
+	
+	public void ChangingToUser(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("User.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	public void ChangingToAdmin(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Admin.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void ChangingToHome(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Home.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void ChangingToFood(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Food.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void ChangingToElectronics(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Electronics.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void ChangingToClothes(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Clothes.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void ChangingToAdminFood(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("AdminFood.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void ChangingToAdminElectronics(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("AdminElectronics.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void ChangingToAdminClothes(ActionEvent event) {
+		try {
+			AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("AdminClothes.fxml"));
+			stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene user = new Scene(userPane);
+			user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(user);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
--- PathaiStore/src/application/Main.java
+++ PathaiStore/src/application/Main.java
@@ -1,48 +1,23 @@
 package application;
 	
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+//import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) throws IOException {
-		//ALL CONTAINERS
-		AnchorPane homePane = (AnchorPane)FXMLLoader.load(getClass().getResource("Home.fxml"));
-		AnchorPane userPane = (AnchorPane)FXMLLoader.load(getClass().getResource("User.fxml"));
-		AnchorPane adminPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Admin.fxml"));
-		AnchorPane foodPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Food.fxml"));
-		AnchorPane electronicsPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Electronics.fxml"));
-		AnchorPane clothesPane = (AnchorPane)FXMLLoader.load(getClass().getResource("Clothes.fxml"));
-		//ALL SCENES
-		Scene home = new Scene(homePane);
-		Scene admin = new Scene(adminPane);
-		Scene user = new Scene(userPane);
-		Scene food = new Scene(foodPane);
-		Scene electronics = new Scene(electronicsPane);
-		Scene clothes = new Scene(clothesPane);
-		//ALL STYLES
-		home.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		admin.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		user.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		food.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		electronics.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		clothes.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		
-		//FIRST SHOWN
-		primaryStage.setScene(home);
-		primaryStage.show();
-		
-		//ALL METHODES
-		
-		
+	public void start(Stage primaryStage) throws Exception {
+			AnchorPane homePane = (AnchorPane)FXMLLoader.load(getClass().getResource("Home.fxml"));
+			Scene home = new Scene(homePane);
+			home.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(home);
+			primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
