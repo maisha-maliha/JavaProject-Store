89a4ca386956b907145ca07ec3e8a046517fba0e
Maisha <officiallymaisha@gmail.com>
2021-04-14 00:07:45 +0600

    created all the scenes and panes

---
+++ PathaiStore/src/application/HomeController.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.event.ActionEvent;
+//import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class HomeController {
+	public void dum(ActionEvent ev) {
+		
+	}
+}
