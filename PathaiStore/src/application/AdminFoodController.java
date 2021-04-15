d6becff6e568617e2dabbef8d8e210dd63bd401f
Maisha <officiallymaisha@gmail.com>
2021-04-15 14:00:07 +0600

    hi

--- PathaiStore/src/application/AdminFoodController.java
+++ PathaiStore/src/application/AdminFoodController.java
@@ -1,6 +1,13 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class AdminFoodController {
 	HomeController home = new HomeController();
@@ -12,12 +19,31 @@ public class AdminFoodController {
 	public void callFood(ActionEvent event) {
 		home.ChangingToAdminFood(event);
 	}
-	public void callClothes(ActionEvent event) {
+	public void callClothes(ActionEvent event) throws Exception {
 		home.ChangingToAdminClothes(event);
+		readsfiles();
 	}
 	public void callElectronics(ActionEvent event) {
 		home.ChangingToAdminElectronics(event);
 	}
 	
+	//========= FOOD PAGE METHODS ==========
+	
+//	BufferReader read = new BufferReader();
+	ArrayList<String> arrlist = new ArrayList<String>(100);
+	
+	public void readsfiles() throws Exception{
+		FileReader fr=new FileReader("src/application/FoodIteamName.txt");    
+        BufferedReader br=new BufferedReader(fr);    
+   int na = br.readLine().length();
+        System.out.println(br.readLine() + " and "+ na);
+        
+        br.close();    
+        fr.close(); 
+	}
+	
+	@FXML
+	public ListView<String> listview;
 
+	
 }
